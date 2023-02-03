4d291c0341757b035dddf18ff71418848f5547a6
chenjiezi <892774940@qq.com>
2023-02-03 16:41:15 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import UnoCSS from "unocss/vite";
+import { presetAttributify, presetUno } from "unocss";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    UnoCSS({
+      presets: [
+        presetAttributify({
+          /* preset options */
+        }),
+        presetUno(),
+        // ...custom presets
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
